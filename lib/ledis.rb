19512317da8e945eab84d0fde0f191bc0cb33925
SuongTrab <suongtran9195@gmail.com>
2019-03-24 00:25:24 +0700

    - public gem ledis

--- lib/ledis.rb
+++ lib/ledis.rb
@@ -4,7 +4,3 @@ require_relative "./ledis/cli"
 require_relative "./ledis/my_string"
 require_relative "./ledis/my_list"
 require_relative "./ledis/my_set"
-
-module Ledis
-  # Your code goes here...
-end
