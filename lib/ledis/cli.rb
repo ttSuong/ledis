ae0e7a0bed867baf079f3cebe90ff102f6f1a033
SuongTrab <suongtran9195@gmail.com>
2019-03-24 12:51:08 +0700

    - update api

--- lib/ledis/cli.rb
+++ lib/ledis/cli.rb
@@ -1,151 +1,218 @@
+require 'json'
 module Ledis
-  def self.author
-    puts 'Suong Tran'
-  end
-  def start(input_value)
-    #----------Project just stop when shutdown server---------#
-    result = {}
-    data_backup = []
-    input_value = input_value.split()
-    arr_value = []
+  class CLI
 
-    input_value.length.times do |idx|
-      arr_value << input_value[idx] if idx > 1
+    def initialize
+      @result = {}
+      @data_backup = []
     end
 
-    if result.key?(input_value[1]) && result[input_value[1]].is_expired
-      result.delete(input_value[1])
-      puts result
-    end
+    def start(input_value)
+      #----------Project just stop when shutdown server---------#
+      is_success = true
+      message = 'SUCCESS'
+      data = []
+      input_value = input_value.split()
+      arr_value = []
 
-    case input_value[0].upcase
-      # snapshot
-    when 'SAVE'
-      data_backup << result
-      puts 'SUCCESS: Backup success'
-    when 'RESTORE'
-      result = data_backup.last
-      puts 'SUCCESS: Restored last version'
-      # summary input
-    when 'KEYS'
-      puts result.keys.join(' ')
-
-    when 'DEL'
-      if result.key?(input_value[1])
-        result.delete(input_value[1])
-        puts "SUCCESS: deleted #{input_value[1]}"
-      else
-        puts 'ERROR: Key not found'
-      end
-
-    when 'FLUSHDB'
-      result = {}
-      puts 'SUCCESS: cleared all keys'
-
-    when 'EXPIRE'
-      expired_time = Time.now + input_value[2].to_i
-      if result.key?(input_value[1])
-        result[input_value[1]].set_timeout(expired_time)
-        puts 'SUCCESS: sat expired time'
-      else
-        puts 'ERROR: Key not found'
-      end
-
-
-    when 'TTL'
-      if result.key?(input_value[1])
-        puts result[input_value[1]].get_timeout
-      else
-        puts 'ERROR: Key not found'
-      end
-
-      # string input
-    when 'SET'
-      if result.key?(input_value[1])
-        puts 'ERROR: Invalid key'
-        next
-      end
-      result[input_value[1]] = MyString.new(input_value[2])
-      puts 'OK'
-
-    when 'GET'
-      puts result[input_value[1]].nil? ? 'ERROR: Key not found'  : result[input_value[1]].get
-
-      #   list input
-    when 'RPUSH'
-      if result.key?(input_value[1])
-        puts 'ERROR: Invalid key'
-        next
-      end
-      result[input_value[1]] = MyList.new(arr_value)
-      puts result[input_value[1]].get_length
-
-    when 'LLEN'
-      puts result[input_value[1]].nil? ? 'ERROR: Key not found'  : result[input_value[1]].get_length
-
-    when 'LPOP'
-      if  result[input_value[1]].nil?
-        puts 'ERROR: Key not found'
-        next
-      end
-
-      puts result[input_value[1]].first_value
-      result[input_value[1]].remove_value(0)
-      puts 'SUCCESS'
-
-    when 'RPOP'
-      if  result[input_value[1]].nil?
-        puts 'ERROR: Key not found'
-        next
-      end
-
-      puts result[input_value[1]].last_value
-      result[input_value[1]].remove_value(result[input_value[1]].get_length - 1)
-      puts 'SUCCESS'
-
-    when 'LRANGE'
-      start = input_value[2].to_i
-      stop = input_value[3].to_i
-      puts result[input_value[1]].get_range(start, stop).join(' ')
-
-      # set input
-    when 'SADD'
-      if result.key?(input_value[1])
-        puts 'ERROR: Invalid key'
-        next
-      end
-
-      result[input_value[1]] = MySet.new(arr_value)
-      puts result[input_value[1]].card
-
-    when 'SCARD'
-      puts result[input_value[1]].nil? ? 'ERROR: Key not found'  : result[input_value[1]].card
-
-    when 'SMEMBERS'
-      puts result[input_value[1]].nil? ? 'ERROR: Key not found'  : result[input_value[1]].member.to_a.join(' ')
-
-    when 'SREM'
-      if result.key?(input_value[1])
-        puts 'ERROR: Invalid key'
-        next
-      end
-      result[input_value[1]].remove_member(arr_value)
-      puts 'SUCCESS'
-
-    when 'SINTER'
-      result_inter  = result[input_value[1]]
       input_value.length.times do |idx|
+        arr_value << input_value[idx] if idx > 1
+      end
 
-        if  result.key?(input_value[idx])
-          puts "ERROR: Invalid key #{input_value[idx]}"
-          result_inter = []
-          break
+      if @result.key?(input_value[1]) && @result[input_value[1]].is_expired
+        @result.delete(input_value[1])
+      end
+
+      case input_value[0].upcase
+        # snapshot
+      when 'SAVE'
+        @data_backup << @result
+        @data_backup
+        @result.each do |key, value|
+          data << {'key': key, 'value': value}
         end
 
-        result_inter = result_inter.inter(result[input_value[idx]]) if idx > 1
+      when 'RESTORE'
+        @result = @data_backup.last
+        data = @result
+        # summary input
+      when 'KEYS'
+        @result.each do |key, value|
+          if @result[key].is_expired
+            @result.delete(key)
+          end
+        end
+        data = {'key': @result.keys}
+
+      when 'DEL'
+        if @result.key?(input_value[1])
+          @result.delete(input_value[1])
+          @result.each do |key, value|
+            data << {'key': key, 'value': value}
+          end
+        else
+          is_success = false
+          message = 'ERROR: Key not found'
+        end
+
+      when 'FLUSHDB'
+        @result = {}
+      when 'EXPIRE'
+        expired_time = Time.now + input_value[2].to_i
+        if @result.key?(input_value[1])
+          @result[input_value[1]].set_timeout(expired_time)
+          data = {'time_expire': @result[input_value[1]].get_timeout}
+        else
+          is_success = false
+          message = 'ERROR: Key not found'
+        end
+
+      when 'TTL'
+        if @result.key?(input_value[1])
+         data =  @result[input_value[1]].get_timeout
+        else
+          is_success = false
+          message = 'ERROR: Key not found'
+        end
+
+        # string input
+      when 'SET'
+        if @result.key?(input_value[1])
+          is_success = false
+          message = 'ERROR: Invalid key'
+        else
+          @result[input_value[1]] = MyString.new(input_value[2])
+          data = {'key' =>  input_value[1], 'value' => @result[input_value[1]].string}
+        end
+
+
+      when 'GET'
+        if @result[input_value[1]].nil?
+          is_success = false
+          message = 'ERROR: Key not found'
+        else
+          is_success = @result[input_value[1]].is_a?(MyString)
+          is_success ? data = @result[input_value[1]].get : message = "ERROR: Wrong key for this type"
+        end
+
+        #   list input
+      when 'RPUSH'
+        if @result.key?(input_value[1])
+          is_success = false
+          message = 'ERROR: Invalid key'
+        else
+          @result[input_value[1]] = MyList.new(arr_value)
+          data = @result[input_value[1]].get_length
+        end
+
+      when 'LLEN'
+        if @result[input_value[1]].nil?
+          is_success = false
+          message = 'ERROR: Key not found'
+        else
+          is_success = @result[input_value[1]].is_a?(MyList)
+          is_success ? data = @result[input_value[1]].get_length :  message = 'ERROR: wrong data type'
+        end
+      when 'LPOP'
+        if  @result[input_value[1]].nil?
+          is_success = false
+          message = 'ERROR: Key not found'
+        else
+          data = @result[input_value[1]].first_value
+          @result[input_value[1]].remove_value(0)
+        end
+
+
+      when 'RPOP'
+        if  @result[input_value[1]].nil?
+          is_success = false
+          message = 'ERROR: Key not found'
+        else
+          data = @result[input_value[1]].last_value
+          @result[input_value[1]].remove_value(@result[input_value[1]].get_length - 1)
+        end
+
+      when 'LRANGE'
+        if @result[input_value[1]].nil?
+          is_success = false
+          message = 'ERROR: Key not found'
+        else
+          start = 0
+          stop = @result[input_value[1]].get_length
+          if input_value[2] && input_value[3]
+            start = input_value[2].to_i
+            stop = input_value[3].to_i
+          end
+          is_success = @result[input_value[1]].is_a?(MyList)
+          is_success ? data = @result[input_value[1]].get_range(start, stop) : message = 'ERROR: Wrong data type'
+
+        end
+
+        # set input
+      when 'SADD'
+        if @result.key?(input_value[1])
+          message = 'ERROR: Invalid key'
+          is_success = false
+        else
+          @result[input_value[1]] = MySet.new(arr_value)
+          data = @result[input_value[1]].card
+        end
+
+      when 'SCARD'
+        if @result[input_value[1]].nil?
+          message = 'ERROR: Key not found'
+          is_success = false
+        else
+          is_success = @result[input_value[1]].is_a?(MySet)
+          is_success ? data = @result[input_value[1]].card : message = 'ERROR: Wrong data type'
+        end
+
+      when 'SMEMBERS'
+        if @result[input_value[1]].nil?
+          message = 'ERROR: Key not found'
+          is_success = false
+        else
+          is_success = @result[input_value[1]].is_a?(MySet)
+          is_success ? data = @result[input_value[1]].member.to_a : message = 'ERROR: Wrong data type'
+        end
+
+      when 'SREM'
+        if !@result.key?(input_value[1])
+          is_success = false
+          message = 'ERROR: Invalid key'
+        else
+          @result[input_value[1]].remove_member(arr_value)
+          data =  @result[input_value[1]]
+        end
+
+
+      when 'SINTER'
+        result_inter  = @result[input_value[1]]
+        input_value.length.times do |idx|
+          if idx > 1
+            if  !@result.key?(input_value[idx])
+              result_inter = []
+              is_success = false
+              message = "ERROR: Key #{input_value[idx]} not found "
+            else
+              is_success = @result[input_value[idx]].is_a?(MySet)
+              message = 'Wrong data type' if !is_success
+              result_inter = result_inter.inter(@result[input_value[idx]]) if is_success
+            end
+            break if !is_success
+          end
+
+        end
+        data = result_inter.member.to_a.sort
       end
-      puts result_inter.member.to_a.sort.join(' ')
+      # end cases
+
+      return_data = {
+          :data => data,
+          :status => is_success,
+          :message => message
+      }
+      return return_data.to_json
     end
-    # end cases
   end
 end
